b1cdcc84baa9c3bd75b0ac63788d41d058641e72
theborgh <borghiborghi@gmail.com>
2021-03-08 20:37:23 +0100

    [39] React-router and page setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import AboutPage from './pages/AboutPage/AboutPage';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import './App.css';
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
+  <>
+    <Header />
+    <Router>
+      <Route path="/" exact component={HomePage} />
+      <Route path="/about" exact component={AboutPage} />
+    </Router>
+    <Footer />
+  </>
 );
 
 export default App;
