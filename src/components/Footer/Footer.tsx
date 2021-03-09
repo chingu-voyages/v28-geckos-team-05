50bbe300057c6aa730e74ecee64d382ee93531b5
JakubKepak <32369029+JakubKepak@users.noreply.github.com>
2021-03-09 18:31:11 +0100

    add footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+
 import './Footer.scss';
 
 export default function Footer() {
-  return <div className="footer">[Footer]</div>;
+  return (
+    <Router>
+      <footer className="footer">
+        <div className="footer__link-container">
+          <a href="https://github.com/chingu-voyages/v28-geckos-team-05">
+            Github
+          </a>
+          <a href="https://spoonacular.com/food-api">Spoonacular API</a>
+          <Link to="/about">Contact Us</Link>
+        </div>
+      </footer>
+    </Router>
+  );
 }
