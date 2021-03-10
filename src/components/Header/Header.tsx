cb2c55e0e73c5d9c94e88c019341f1841ccd38fe
theborgh <borghiborghi@gmail.com>
2021-03-10 13:09:06 +0100

    Add initial desktop header setup

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,45 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faHome } from '@fortawesome/free-solid-svg-icons';
 import './Header.scss';
 
 function Header() {
+  const toggleMobileMenu = () => {};
+
   return (
-    <div className="header">
-      <Link to="/">Home</Link> | <Link to="/about">About</Link>
-    </div>
+    <nav className="navbar">
+      <FontAwesomeIcon
+        icon={faBars}
+        size="2x"
+        onClick={toggleMobileMenu}
+        id="hamburgerMenu"
+        className="navbar-menuIcon"
+      />
+      <div className="navbar-links">
+        <Link to="/">
+          <FontAwesomeIcon
+            icon={faHome}
+            size="2x"
+            className="navbar-menuItem"
+          />
+        </Link>
+        <Link to="/about" className="navbar-menuItem">
+          About
+        </Link>
+        <Link to="/calendar" className="navbar-menuItem">
+          Calendar
+        </Link>
+        <Link to="/favorites" className="navbar-menuItem">
+          Favorites
+        </Link>
+
+        <div className="navbar-content--right">
+          Hi, User123
+          <Link to="#">Login</Link>
+        </div>
+      </div>
+    </nav>
   );
 }
 
