2856f0ec18d740ccbd92c6b10f0745fd6f670628
JakubKepak <32369029+JakubKepak@users.noreply.github.com>
2021-03-21 18:35:17 +0100

    add singup page

--- src/components/Signup/Signup.tsx
+++ src/components/Signup/Signup.tsx
@@ -1,5 +1,59 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useState } from 'react';
+import './Signup.scss';
 
 export default function Signup() {
-  return <div>Sign up</div>;
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [passwordConfirmed, setPasswordConfirmed] = useState<string>('');
+
+  const signUpHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (password !== passwordConfirmed) {
+      console.log('passwords does not match');
+      return;
+    }
+    console.log('sign up');
+  };
+
+  return (
+    <form className="signup__form-container" onSubmit={signUpHandler}>
+      <div className="signup__input-container">
+        <label htmlFor="username">email: </label>
+        <input
+          className="signup__input"
+          type="email"
+          id="username"
+          name="username"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="signup__input-container">
+        <label htmlFor="username">password: </label>
+        <input
+          className="signup__input"
+          type="password"
+          id="password"
+          name="password"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="signup__input-container">
+        <label htmlFor="username">confirm password: </label>
+        <input
+          className="signup__input"
+          type="password"
+          id="passwordConfirmed"
+          name="passwordConfirmed"
+          required
+          onChange={(e) => setPasswordConfirmed(e.target.value)}
+        />
+      </div>
+      <button className="login__button" type="submit">
+        Sign Up
+      </button>
+    </form>
+  );
 }
