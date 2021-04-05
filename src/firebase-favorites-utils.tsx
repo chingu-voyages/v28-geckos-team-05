323c6da373e0ef8b066abfa3a201bb1f91d77db8
theborgh <borghiborghi@gmail.com>
2021-04-05 08:47:50 +0200

    Setup 2

---
+++ src/firebase-favorites-utils.tsx
@@ -0,0 +1,45 @@
+import { Recipe } from './typescript/types';
+import { db, auth } from './firebase';
+
+export const getUserId = () => {
+  if (auth.currentUser !== null) {
+    return auth.currentUser?.uid;
+  }
+  return null;
+};
+
+export const storeFavorite = async (recipe: Recipe, userId: string) => {
+  const userRef = await db.collection('user-data').doc(userId);
+  const favoritesRef = await userRef.collection('favorites');
+  const id = recipe.id.toString();
+
+  await favoritesRef.doc(id).set({
+    id: recipe.id,
+    title: recipe.title,
+    image: recipe.image,
+    readyInMinutes: recipe.readyInMinutes,
+    servings: recipe.servings,
+    aggregateLikes: recipe.aggregateLikes,
+  });
+};
+
+export const getFavorites = async (userId: string) => {
+  const userRef = await db.collection('user-data').doc(userId);
+  const favoritesRef = await userRef.collection('favorites');
+  const snapshot = await favoritesRef.get();
+  const favorites: Recipe[] = [];
+
+  snapshot.forEach((doc) => {
+    favorites.push(doc.data() as Recipe);
+  });
+
+  return favorites.map((rec) => ({
+    ...rec,
+    aggregateLikes: String(rec.aggregateLikes),
+  }));
+};
+
+export const removeFromFavorites = async (userId: string, recipeId: string) => {
+  const userRef = await db.collection('user-data').doc(userId);
+  await userRef.collection('favorites').doc(recipeId).delete();
+};
--- src/pages/FavoritesPage/FavoritesPage.tsx
+++ src/pages/FavoritesPage/FavoritesPage.tsx
@@ -1,5 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Recipe } from '../../typescript/types';
+import {
+  getUserId,
+  getFavorites,
+  removeFromFavorites,
+} from '../../firebase-favorites-utils';
+import RecipeCard from '../../components/RecipeCard/RecipeCard';
 
 export default function FavoritesPage() {
-  return <div className="page">FAVORITES</div>;
+  const [recipesList, setRecipesList] = useState<Recipe[]>([]);
+  const userId = getUserId();
+
+  useEffect(() => {
+    async function getFavoritesFromDB() {
+      if (userId) {
+        setRecipesList(await getFavorites(userId));
+      }
+    }
+
+    getFavoritesFromDB();
+  }, [userId]);
+
+  const handleClickRemove = (recipeId: number) => {
+    !!userId && removeFromFavorites(userId, String(recipeId));
+    setRecipesList(recipesList.filter((recipe) => recipe.id !== recipeId));
+  };
+
+  return (
+    <div className="page">
+      <h1 className="page__title">Favorites</h1>
+      <div className="recipes__container">
+        {recipesList.map((recipe: Recipe) => (
+          <RecipeCard key={recipe.id} recipe={recipe} />
+        ))}
+      </div>
+    </div>
+  );
 }
