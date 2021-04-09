c94c6540ea5fb398dc9bf03b674195cad856c5d9
JakubKepak <32369029+JakubKepak@users.noreply.github.com>
2021-04-09 14:31:24 +0200

    add login/signup interaction

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,58 +1,72 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { auth } from '../../firebase/firebase';
 import './Login.scss';
 
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const history = useHistory();
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const loginHandler = async (e: any) => {
     e.preventDefault();
+    setLoading(true);
     try {
       await auth.signInWithEmailAndPassword(email, password);
+      history.push('/');
     } catch (err) {
-      // eslint-disable-next-line no-console
-      console.error(err);
+      setError(err.message);
     }
+    setLoading(false);
   };
 
   return (
-    <form className="login__form-container">
-      <div className="login__input-container">
-        <label htmlFor="username">email: </label>
-        <input
-          className="login__input"
-          type="email"
-          id="username"
-          name="username"
-          required
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">password: </label>
-        <input
-          className="login__input"
-          type="password"
-          id="password"
-          name="password"
-          required
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
+    <>
+      <form className="login__form-container">
+        <div className="login__input-container">
+          <label htmlFor="username">email: </label>
+          <input
+            className="login__input"
+            type="email"
+            id="username"
+            name="username"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">password: </label>
+          <input
+            className="login__input"
+            type="password"
+            id="password"
+            name="password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
 
-      <button className="login__button" type="submit" onClick={loginHandler}>
-        Log in
-      </button>
-      <div className="login__singup-text">
-        <span>Don't have account?</span>
-        <Link to="/signup">
-          <span className="login__singup-text__link">Sign In</span>
-        </Link>
-      </div>
-    </form>
+        <button
+          disabled={loading}
+          className="login__button"
+          type="submit"
+          onClick={loginHandler}
+        >
+          {loading ? <span>Logging in ...</span> : <span>Log in</span>}
+        </button>
+        <div className="login__singup-text">
+          <span>Don't have account?</span>
+          <Link to="/signup">
+            <span className="login__singup-text__link">Sign In</span>
+          </Link>
+        </div>
+      </form>
+      {error && <div className="login__error">{error}</div>}
+    </>
   );
 }
--- src/components/Signup/Signup.tsx
+++ src/components/Signup/Signup.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { auth } from '../../firebase/firebase';
 import './Signup.scss';
 
@@ -8,59 +9,67 @@ export default function Signup() {
   const [password, setPassword] = useState<string>('');
   const [passwordConfirmed, setPasswordConfirmed] = useState<string>('');
   const [error, setError] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const history = useHistory();
 
   const signUpHandler = async (e: React.FormEvent) => {
     e.preventDefault();
+    setLoading(true);
     if (password !== passwordConfirmed) {
       setError('passwords does not match');
       return;
     }
     try {
       await auth.createUserWithEmailAndPassword(email, password);
+      history.push('/');
     } catch (err) {
       setError(err.message);
     }
+    setLoading(false);
   };
 
   return (
-    <form className="signup__form-container" onSubmit={signUpHandler}>
+    <>
+      <form className="signup__form-container" onSubmit={signUpHandler}>
+        <div className="signup__input-container">
+          <label htmlFor="username">email: </label>
+          <input
+            className="signup__input"
+            type="email"
+            id="username"
+            name="username"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="signup__input-container">
+          <label htmlFor="username">password: </label>
+          <input
+            className="signup__input"
+            type="password"
+            id="password"
+            name="password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="signup__input-container">
+          <label htmlFor="username">confirm password: </label>
+          <input
+            className="signup__input"
+            type="password"
+            id="passwordConfirmed"
+            name="passwordConfirmed"
+            required
+            onChange={(e) => setPasswordConfirmed(e.target.value)}
+          />
+        </div>
+        <button className="login__button" type="submit">
+          {loading ? <span>Signing up...</span> : <span>Sign up</span>}
+        </button>
+      </form>
       {error && <div className="signup__error-container">{error}</div>}
-      <div className="signup__input-container">
-        <label htmlFor="username">email: </label>
-        <input
-          className="signup__input"
-          type="email"
-          id="username"
-          name="username"
-          required
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div className="signup__input-container">
-        <label htmlFor="username">password: </label>
-        <input
-          className="signup__input"
-          type="password"
-          id="password"
-          name="password"
-          required
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <div className="signup__input-container">
-        <label htmlFor="username">confirm password: </label>
-        <input
-          className="signup__input"
-          type="password"
-          id="passwordConfirmed"
-          name="passwordConfirmed"
-          required
-          onChange={(e) => setPasswordConfirmed(e.target.value)}
-        />
-      </div>
-      <button className="login__button" type="submit">
-        Sign Up
-      </button>
-    </form>
+    </>
   );
 }
